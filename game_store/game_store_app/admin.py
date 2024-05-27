ef14adfbf29292e0ad9c6c37a2c8330a123c6b75
Volodymyr-Is <144656592+Volodymyr-Is@users.noreply.github.com>
2024-05-27 09:31:27 +0300

    Delete game_store directory

--- game_store/game_store_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from game_store_app.models import *
-
-# Register your models here.
-admin.site.register(Platform)
-admin.site.register(Genre)
-admin.site.register(Developer)
-admin.site.register(Tag)
-admin.site.register(Game)
--- game_store/game_store_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GameStoreAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'game_store_app'
